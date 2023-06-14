7408a188160b5c839543874029164965ca6c561d
kibuchi muchai <kibuchimuchai@gmail.com>
2023-06-15 00:10:55 +0300

    created a java project

---
+++ app/src/main/java/java/programming/App.java
@@ -0,0 +1,11 @@
+/*
+ * Write a Java program that asks the user to enter their sur name and current age then print the number of characters of their sir name and even or odd depending on their age number
+ */
+package java.programming;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+}}
